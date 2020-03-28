9d0ed7297038690faa9834756b089cc88a22145c
italo <contato.italo1020@gmail.com>
2020-03-27 23:52:57 -0300

    criacao da api para se comunicar com o frontend. Foi feito a lógica de cadastro de ONGS.

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,34 +1,84 @@
-import React from 'react';
-import './styles.css';
+import React, { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import api from '../../services/api';
+import './styles.css';
 
-export default function Register(){
+export default function Register() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf,  setUf] = useState('');
+
+    const history = useHistory();
+
+
+    async function handleRegister(e) {
+        e.preventDefault();
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        };
+        try {
+            const response = await api.post('ongs', data);
+            alert(`Seu ID de acesso: ${response.data.id}`);
+            history.push('/');
+        } catch(err){
+            alert(`Erro no cadastro, tente novamente`)
+        }
+
+    }
     return (
         <div className="register-container">
             <div className="content">
-                 <section>
-                     <img src= {logoImg} alt="Be The Hero"/>
-                     <h1>Cadastro</h1>
-                     <p>Faça seu cadastro, entre na plataforma e ajude a encontrar os casos da sua ONG.</p>
-                     <Link className="back-link" to="/">
+                <section>
+                    <img src={logoImg} alt="Be The Hero" />
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude a encontrar os casos da sua ONG.</p>
+                    <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#e02041" />
                         Voltar pra home</Link>
-                 </section>
-                 
-                 <form >
-                     <input placeholder="Nome da ONG"/>
-                     <input type="email" placeholder="E-mail"/>
-                     <input placeholder="WhatsApp"/>
+                </section>
 
-                     <div className="input-group">
-                        <input placeholder="Cidade"/>
-                        <input placeholder="UF" style = {{ width: 80 }}/>
-                     </div>
+                <form onSubmit = {handleRegister}>
+                    <input
+                        placeholder="Nome da ONG"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
 
-                <buton className="button" type="submit">Cadastrar</buton>
-                 </form>
+                    <input type="email"
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <input
+                        placeholder="WhatsApp"
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}
+                    />
+
+                    <div className="input-group">
+                        <input
+                            placeholder="Cidade"
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+
+                        <input
+                            placeholder="UF" style={{ width: 80 }} minLength="2" maxLength="2"
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />
+                    </div>
+                    <button className="button" type="submit">Cadastrar</button>
+                </form>
             </div>
         </div>
     );
